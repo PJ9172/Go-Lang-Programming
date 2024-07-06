079efa0680dea0990762834e751002b55f9f1888
Prajwal <rohitjagtap5000@gmail.com>
2024-07-07 01:45:46 +0530

    First Commit

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+import ("fmt" "math")
+func main()  {
+	// fmt.Println("Hello pj...")
+	//num:=12
+	id , name :=101,"Prajwal" 
+	fmt.Print("Id : ",id, "\n","NAME :" ,name)
+	n:=4.0
+	fmt.Print("\n",math.Sqrt(n))
+
+}
