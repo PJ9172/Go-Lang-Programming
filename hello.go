e36198b1d1bf2e738ec206d14ab9580fc46a38aa
Prajwal <rohitjagtap5000@gmail.com>
2024-07-20 20:19:17 +0530

    Variables added !!!

--- hello.go
+++ hello.go
@@ -1,11 +1,10 @@
 package main
-import ("fmt" "math")
+import ("fmt")
 func main()  {
 	// fmt.Println("Hello pj...")
-	//num:=12
-	id , name :=101,"Prajwal" 
-	fmt.Print("Id : ",id, "\n","NAME :" ,name)
-	n:=4.0
-	fmt.Print("\n",math.Sqrt(n))
+	var p1 = "hello"
+	fmt.Println(p1)
+	// p1=105
+	// fmt.Println(p1)
 
 }
