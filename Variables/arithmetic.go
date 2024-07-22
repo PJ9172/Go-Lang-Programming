b024125dcbf6b45536482246d76e0f19ed8b6df1
Prajwal <rohitjagtap5000@gmail.com>
2024-07-22 17:00:57 +0530

    Variable theory & practical Questions

---
+++ Variables/arithmetic.go
@@ -0,0 +1,13 @@
+// Write a program that takes two integers as input and performs 
+//addition, subtraction, multiplication, and division, then prints the results.
+
+package main
+import "fmt"
+func main() {
+	num1, num2 := 80, 40
+	fmt.Println("Num 1 :",num1,", Num 2 :",num2)
+	fmt.Println("Additon : ",num1+num2)
+	fmt.Println("Substraction : ",num1-num2)
+	fmt.Println("Multiplication : ",num1*num2)
+	fmt.Println("Division : ",num1/num2)
+}
---
+++ Variables/swap.go
@@ -0,0 +1,14 @@
+//Write a program that takes two numbers as input, swaps their values, and prints the swapped values.
+
+package main
+// import "fmt"
+// func main() {
+// 	a, b := 10,20
+// 	var t int  
+// 	fmt.Println("a :",a,", b :",b)
+// 	fmt.Println("After swapping : ")
+// 	t=a
+// 	a=b
+// 	b=t
+// 	fmt.Println("a :",a,", b :",b)
+// }
