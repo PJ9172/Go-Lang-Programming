9aec51e3481905b253963a02edd772e71519a31f
Prajwal <rohitjagtap5000@gmail.com>
2024-08-04 00:40:17 +0530

    Array Practical added!!!

---
+++ Array/average.go
@@ -0,0 +1,17 @@
+//Write a program that calculates the average of all elements in an array.
+
+package main
+
+// import "fmt"
+
+// func main() {
+// 	var arr [5]int
+// 	fmt.Print("Enter the numbers in array : ")
+// 	var i int
+// 	var sum = 0
+// 	for i = 0; i < 5; i++ {
+// 		fmt.Scan(&arr[i])
+// 		sum = sum + arr[i]
+// 	}
+// 	fmt.Print("Average is : ", sum/5)
+// }
---
+++ Array/maximum.go
@@ -0,0 +1,19 @@
+//Write a program that finds the maximum element in an array.
+
+package main
+
+import "fmt"
+
+func main() {
+	var max = 0
+	var i int
+	var arr [5]int
+	fmt.Print("Enter the 5 numbers in array : ")
+	for i = 0; i < 5; i++ {
+		fmt.Scan(&arr[i])
+		if max < arr[i] {
+			max = arr[i]
+		}
+	}
+	fmt.Print("Maximum of array is : ", max)
+}
